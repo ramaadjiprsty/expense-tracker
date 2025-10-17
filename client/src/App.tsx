deef167cc482c53efb4ae222a7844388b4629800
Rama <ramaadjiprsty@gmail.com>
2025-10-18 03:53:34 +0700

    feat(client): setup react + typescript + tailwind frontend via vite

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Halo, Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
